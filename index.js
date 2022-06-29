33259e6bbd272896224993f678599c16aa9f57e7
Mayanksandikar <mayanksandikar191098@gmail.com>
2022-06-29 19:18:36 +0530

    new

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+const http =require("http");
+const server = http.createServer(function(req,res){
+    res.write("<h1>Welcome to Node js</h1>");
+    res.write("This is first HTTP server assignment</br></br>")
+    res.write(JSON.stringify({Name:"Mayank Sandikar"}));
+    res.end()
+})
+server.listen(4000)
